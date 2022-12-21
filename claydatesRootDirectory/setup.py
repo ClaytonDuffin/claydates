004d203d23338b535780e9a5c910c56c6562eec8
Clayton Duffin <clayduffin@gmail.com>
2022-12-21 17:39:40 -0500

    Update setup.py

--- claydatesRootDirectory/setup.py
+++ claydatesRootDirectory/setup.py
@@ -1,13 +1,12 @@
+from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / 'README.md').read_text()
 
-from setuptools import setup, find_packages
 setup(
       name='claydates',
-      version='1.0.3',
+      version='1.0.4',
       description='Package used for cleaning, restructuring, logging, and plotting of financial data retrieved from the Twelve Data API.',
-      long_description=long_description,
+      long_description=(this_directory / 'README.md').read_text(),
       long_description_content_type='text/markdown',
       url='https://github.com/ClaytonDuffin/claydates',
       readme='README.md',
